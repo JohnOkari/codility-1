86fbfb667bfc9f1ca31240c7c03832528bdeb470
JohnOkari <johnnokari@gmail.com>
2023-02-23 16:09:40 +0300

    add puts

--- comparenumbers.rb
+++ comparenumbers.rb
@@ -1,14 +1,15 @@
 def compare_numbers(a, b)
     case a <=> b 
     when 1
-        "a is greater than b"
+        "#{a} is greater than #{b}"
     when -1
-        "a is smaller than b"
+        "#{a} is smaller than #{b}"
     else
-        "a is equal to b"
+        "#{a} is equal to #{b}"
     end
-
-   
 end
 
 puts compare_numbers(5,4)
+puts compare_numbers(1,4)
+puts compare_numbers(-3,4)
+puts compare_numbers(5,9)
